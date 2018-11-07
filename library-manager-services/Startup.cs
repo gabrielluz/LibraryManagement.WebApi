a54156239da53aa16bca481be32c8a17ac49987d
Gabriel Camlofski Luz <gabi_cluz@hotmail.com>
2018-11-07 00:41:43 -0200

    Remove the 'UseExceptionHandler' to avoid 404 status when there is an internal error (500).

--- library-manager-services/Startup.cs
+++ library-manager-services/Startup.cs
@@ -37,7 +37,6 @@ namespace CSG_Library_Management
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseExceptionHandler("/Error");
             app.UseHsts();
             app.UseHttpsRedirection();
             app.UseMvc();
