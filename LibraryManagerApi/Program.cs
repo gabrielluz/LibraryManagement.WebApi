df9e50a71bf3b1cb896957c63059a3f5bc1ac0ac
Gabriel Camlofski Luz <gabi_cluz@hotmail.com>
2018-11-27 23:28:57 -0200

    Fix namespace name for auto-generated (scaffold) classes.

--- LibraryManagerApi/Program.cs
+++ LibraryManagerApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CSG_Library_Management
+namespace LibraryManagerApi
 {
     public class Program
     {
