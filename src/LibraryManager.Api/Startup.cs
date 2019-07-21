d60c1998f64a8ad24b8f121e5f50a405e818c1de
Gabriel Camlofski Luz <gabi_cluz@hotmail.com>
2019-07-21 11:50:32 -0300

    Remove the connection string from appsettings file
    The connection string has been added to the environment
    variables in the SO. The database password has been changed
    due to the versioned connection string in Github.

--- src/LibraryManager.Api/Startup.cs
+++ src/LibraryManager.Api/Startup.cs
@@ -33,7 +33,7 @@ namespace LibraryManager.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration.GetConnectionString("LibraryManager.ApiConnection");
+            var connectionString = Configuration.GetConnectionString("LibraryManagerApiConnection");
             services.ConfigureDatabase(connectionString);
             services.ConfigureDependencyInjection();
             services.ConfigureMvc();
